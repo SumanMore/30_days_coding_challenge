befc21c57515ba409887e00ab14a344c2e2568eb
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-06-11 19:39:09 +0530

    Add files via upload

---
+++ 30 days of coding challenge/q54.java
@@ -0,0 +1,31 @@
+public class q54 {
+    public static void main(String args[])
+    {
+        int m=5;
+        String s="";
+        for(int i=1;i<=3;i++)
+        {System.out.print(s);
+            for(int j=1;j<=m;j++ )
+            {
+            System.out.print("*");
+           
+           }
+           m-=2;
+           System.out.println();
+           s=s+" ";
+          
+        }
+    System.out.print(" ");
+    int m1=3;
+    for(int i=1;i<=2;i++)
+    {
+for(int j=1;j<=m1;j++)
+{
+    System.out.print("*");
+}
+    m1+=2;
+System.out.println();
+    }
+}
+    
+}
