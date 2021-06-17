a85145d02148712c2feb16e60b610f28c04973d8
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-06-17 20:41:10 +0530

    Add files via upload

---
+++ 30 days of coding challenge/q22.java
@@ -0,0 +1,35 @@
+ public class q22{
+    public static void main(String[] args)
+     {
+      long x=0b1101,y=0b1001;
+      
+     System.out.println(Long.toBinaryString(multiply(x,y)));
+     
+  }
+    public static long multiply(long x,long y)
+     {
+      long res =0,k=0,c=0;
+      while (x !=0) 
+      {
+        if ((x & 1) != 0)
+        {
+          c++;
+        res = add(res , y<<k);
+        }
+      k=k+1;
+      x>>=1;}
+      
+      return res;
+      }  
+      public static long add(long res,long y) 
+      { 
+        long carry=0;
+        while(y!=0)
+        {
+          carry=res&y;
+          res=res^y;
+          y=carry<<1;
+        }
+  return res;
+      }}
+  
