70b09e6ee46aef1a4736b042ee21ee93c1510060
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-06-17 20:02:30 +0530

    Add files via upload

---
+++ 30 days of coding challenge/q7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+public class q7 {
+    public static void main(String args[])
+    {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the side of square");
+double a=sc.nextDouble();
+double area = (Math.PI * a * a) / 4;
+System.out.println(area);
+sc.close();
+    }
+    
+}
