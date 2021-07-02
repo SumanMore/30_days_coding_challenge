90b11516da525c4f7f21b68069a5f68dcbc03516
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-07-02 11:19:58 +0530

    Update q93.java

--- 30 days of coding challenge/q93.java
+++ 30 days of coding challenge/q93.java
@@ -4,10 +4,10 @@ public class q93
 {
     public static void main(String args[])
     {
-        String temp = BigInteger.ONE.shiftLeft(1000).toString();
+        String s = BigInteger.ONE.shiftLeft(1000).toString();
 		int sum = 0;
-		for (int i = 0; i < temp.length(); i++)
-			sum += temp.charAt(i) - '0';
+		for (int i = 0; i < s.length(); i++)
+			sum += s.charAt(i) - '0';
 		System.out.println( Integer.toString(sum));
     }
 }
