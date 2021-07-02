043714ee18b97b90490791ca44dae9d02989a2c5
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-07-02 11:18:57 +0530

    Add files via upload

---
+++ 30 days of coding challenge/q93.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class q93 
+{
+    public static void main(String args[])
+    {
+        String temp = BigInteger.ONE.shiftLeft(1000).toString();
+		int sum = 0;
+		for (int i = 0; i < temp.length(); i++)
+			sum += temp.charAt(i) - '0';
+		System.out.println( Integer.toString(sum));
+    }
+}
